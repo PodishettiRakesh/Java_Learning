d6c8109774da303c0d5bbecb9b63ebd259cad7b7
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-21 22:08:07 +0530

    method to print sum of two numbers

--- Demo.java
+++ Demo.java
@@ -1,5 +1,14 @@
+// public class Demo{
+//     public static void main(String[] args){
+//         System.out.println("Hello world!");
+//     }
+// }
+
 public class Demo{
-    public static void main(String[] args){
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        int x=6;
+        int y=8;
+        int sum=x+y;
+        System.out.println(sum);
     }
 }
