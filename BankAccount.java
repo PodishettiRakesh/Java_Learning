0b7ca396d4dfe032006040e178f624cfb42e5b3d
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-25 22:58:04 +0530

    initialized the fields with default construtor

--- BankAccount.java
+++ BankAccount.java
@@ -2,4 +2,11 @@ public class BankAccount {
     String HolderName;
     String AcNumber;
     double Balence;
+
+    public BankAccount(String HolderName,String AcNumber,double Balence){
+        this.HolderName=HolderName;
+        this.AcNumber=AcNumber;
+        this.Balence=Balence;
+
+    }
 }
