1b5fd23c6d88653ea3f0051a25d660b7f3596489
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-25 22:42:22 +0530

    added some more attributes

--- Student.java
+++ Student.java
@@ -2,11 +2,15 @@ public class Student {
     int student_id;
     String name;
     String grade;
+    int age;
+    float marks;
 
-    public Student(int id,String name,String grade){
+    public Student(int id,String name,String grade,int age,float marks){
         this.student_id=id;
         this.name=name;
         this.grade=grade;
+        this.age=age;
+        this.marks=marks;
     }
 
 
