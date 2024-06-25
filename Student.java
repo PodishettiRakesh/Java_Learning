652c781daaa95147137ed62ba70257bb73744a6a
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-25 22:34:11 +0530

    object created as rakesh which Student type

--- Student.java
+++ Student.java
@@ -1,13 +1,14 @@
 public class Student {
     int student_id;
     String name;
-    char grade;
+    String grade;
 
-    public Student(int id,String name,char grade){
+    public Student(int id,String name,String grade){
         this.student_id=id;
         this.name=name;
         this.grade=grade;
     }
+
 
     public void Studentdetails(){
         System.out.println("student id is: "+ student_id);
@@ -16,7 +17,10 @@ public class Student {
     }
 
     public static void main(String[] args) {
-        
+        Student rakesh = new Student(01,"Rakesh","A"){
+            x=rakesh.Studentdetails();
+
+        }
     }
 
 
