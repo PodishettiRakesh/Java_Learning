c1dd6dfa287a795f51eea44a6b8ef40ce3ee3c5a
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-26 15:55:41 +0530

    college maked it as static

--- Rakesh/Students.java
+++ Rakesh/Students.java
@@ -3,14 +3,17 @@ package Rakesh;
 public class Students {
     int student_id;
     String name;
-    String college;
+    static String college=;
 
 
-    public Students(int student_id,String name,String college){
+    public Students(int student_id,String name){
         this.student_id=student_id;
         this.name=name;
-        this.college=college;
     }
+
+    // public void setCollege(String clg){
+    //     Students.college=clg;
+    // }
 
      public void displaydetails(){
         System.out.println("student id is: "+ student_id);
--- main.java
+++ main.java
@@ -2,7 +2,7 @@ import Rakesh.Students;
 
 public class main {
     public static void main(String[] args) {
-        Students s1=new Students(01, "podishetti rakesh ", "iiit-H");
+        Students s1=new Students(01, "podishetti rakesh ");
         s1.displaydetails();
     }
 }
