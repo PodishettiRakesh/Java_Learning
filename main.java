f94e49d6008572f93c1c33dc116820198ce301ef
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-26 15:58:17 +0530

    displaying details of students after setting the college name

--- main.java
+++ main.java
@@ -4,5 +4,8 @@ public class main {
     public static void main(String[] args) {
         Students s1=new Students(01, "podishetti rakesh ");
         s1.displaydetails();
+
+        s1.setCollege("IIIT-H");
+        s1.displaydetails();
     }
 }
