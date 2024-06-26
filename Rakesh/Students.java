b5ba36e784102cf109e6d59956a73949247efe47
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-26 15:27:48 +0530

    package created with rakesh name

--- Rakesh/Students.java
+++ Rakesh/Students.java
@@ -1,5 +1,16 @@
 package Rakesh;
 
 public class Students {
+    int student_id;
+    String name;
+    static String college;
+
+    public Students(int student_id,String name){
+        this.student_id=student_id;
+        this.name=name;
+        this.college="IITH";
+    }
+
     
 }
+
