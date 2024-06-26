fe1ac268b9cd985a8b3e3e9cac6851618e47f910
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-26 15:37:19 +0530

    created new object for imported student package

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ import Rakesh.Students;
 
 public class main {
     public static void main(String[] args) {
-        Students s1=new Students(01, "podishetti rakesh ", "iit-H");
+        Students s1=new Students(01, "podishetti rakesh ", "iiit-H");
         s1.Studentdetails();
     }
 }
