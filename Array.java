01461c7c76bbc6ca4b16b50286bfe4da3eded18d
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-01 23:53:07 +0530

    object creation to generate random numbers

--- Array.java
+++ Array.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class Array {
@@ -6,13 +5,14 @@ public class Array {
     private int numValues;
     private int maxValues;
 
-   
+    // Default constructor
     public Array() {
         this.numValues = 0;
         this.maxValues = 20;
         this.intArray = new int[maxValues];
     }
 
+    // Constructor with an array parameter to remove duplicates
     public Array(int[] arr) {
         this.intArray = new int[20];
         this.numValues = 0;
@@ -30,23 +30,25 @@ public class Array {
         }
     }
 
-    public Array(int numRandom, int low, int high){
-        this.intArray=new int[20];
-        if (numRandom>=low && numRandom<=high){
-            int count=0;
-            for(int i=0;i<numRandom;i++){
-                Random ran = new Random();
-                int ele=ran.nextInt(high-low-1)+low;
+    // Constructor to generate random numbers within a specified range
+    public Array(int numRandom, int low, int high) {
+        this.intArray = new int[20]; 
+        this.numValues = 0; 
+        this.maxValues = 20; 
 
-                if(count<this.maxValues){
-                    intArray[count++]=ele;
-                    numValues++;
-                }
+        if (numRandom >= 1 && low <= high) {
+            Random ran = new Random();
+            int count = 0;
+            while (count < numRandom && numValues < maxValues) {
+                int ele = ran.nextInt(high - low + 1) + low; 
+                intArray[numValues++] = ele;
+                count++;
             }
+        } else {
+            System.out.println("Invalid input parameters."); 
         }
     }
 
-    
     public void printArray() {
         for (int i = 0; i < numValues; i++) {
             System.out.print(intArray[i] + " ");
@@ -58,9 +60,9 @@ public class Array {
         Array ar = new Array();
         int[] arr = {1, 2, 3, 4, 2, 3, 5, 6, 7, 9, 1};
         Array myArray = new Array(arr);
-        myArray.printArray(); 
-        Array ranArray = new Array(12,2,9);
-        ranArray.printArray();
+        myArray.printArray();
 
+        Array ranArray = new Array(12, 2, 9); 
+        ranArray.printArray(); 
     }
 }
