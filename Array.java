449f8023977b0f1735c777d1ec1c05e9873a312c
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-01 23:01:34 +0530

    constructor to create unique numbers from array and with non-destructive of array

--- Array.java
+++ Array.java
@@ -1,23 +1,46 @@
-public class Array{
+import java.util.Arrays;
+
+public class Array {
     private int[] intArray;
     private int numValues;
     private int maxValues;
 
-    public Array(){
-        this.numValues=0;
-        this.maxValues=20;
-        this.intArray= new int[maxValues]; 
+   
+    public Array() {
+        this.numValues = 0;
+        this.maxValues = 20;
+        this.intArray = new int[maxValues];
     }
 
-    public int[] MyArray(int[] arr){
-        int[] array=new int[20];
-        return array;
+    public Array(int[] arr) {
+        this.intArray = new int[20];
+        this.numValues = 0;
+        this.maxValues = 20;
+        for (int i = 0; i < arr.length; i++) {
+            int repeated = 0;
+            for (int j = 0; j < numValues; j++) {
+                if (arr[i] == intArray[j]) {
+                    repeated += 1;
+                }
+            }
+            if (repeated == 0 && numValues < maxValues) {
+                intArray[numValues++] = arr[i];
+            }
+        }
+    }
+
+    
+    public void printArray() {
+        for (int i = 0; i < numValues; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         Array ar = new Array();
-        int[] arr= {1,2,1,3,4,4,5,6,7};
-        System.out.println(ar.MyArray(arr));
-
+        int[] arr = {1, 2, 3, 4, 2, 3, 5, 6, 7, 9, 1};
+        Array myArray = new Array(arr);
+        myArray.printArray(); 
     }
 }
