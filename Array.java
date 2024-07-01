df68674231e046738ebdff4e66be2a50ed18c5ef
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-01 21:52:16 +0530

    constructed MyArray object with capacity 20

--- Array.java
+++ Array.java
@@ -8,4 +8,15 @@ public class Array{
         this.maxValues=20;
         this.intArray= new int[maxValues]; 
     }
+
+    public int[] MyArray(int[] arr){
+        int[] array=new int[20];
+        return array;
+    }
+
+    public static void main(String[] args) {
+        Array ar = new Array();
+        System.out.println(ar);
+        
+    }
 }
