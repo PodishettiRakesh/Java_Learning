664a8cab42c64c9559e52855c676558bba479c0e
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-01 21:41:16 +0530

    default constructer initilizing the fields of class Array

--- Array.java
+++ Array.java
@@ -2,6 +2,10 @@ public class Array{
     private int[] intArray;
     private int numValues;
     private int maxValues;
-    
 
+    public Array(){
+        this.numValues=0;
+        this.maxValues=20;
+        this.intArray= new int[maxValues]; 
+    }
 }
