d2eda7d8a05493e193613ecfabdb6788ebfe4192
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-04 21:55:10 +0530

    fields are initialized in Tic_Tac_Toe class

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,3 +1,9 @@
 public class Tic_Tac_Toe {
-    
+    // size is 3
+    private static final int size=3;
+    private static  final char player1='x';
+    private static final char player2='o';
+
+    private char[] board;
+
 }
