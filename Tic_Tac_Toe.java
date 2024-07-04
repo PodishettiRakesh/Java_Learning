075a90f5eab78122f81cad7d7ea576a39cbd9cdc
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-04 22:16:02 +0530

    initialized board is implemented with empty spaces

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,9 +1,24 @@
+import java.util.Random;
 public class Tic_Tac_Toe {
     // size is 3
-    private static final int size=3;
+    private static final int SIZE=3;
     private static  final char player1='x';
     private static final char player2='o';
 
-    private char[] board;
+    private char[][] board;
 
+    public Tic_Tac_Toe(){
+        initializeBoard();
+    }
+
+    private void initializeBoard(){
+        board= new char[SIZE][SIZE];
+        // Random ran = new Random();
+        for(int i=0;i<SIZE;i++){
+            for(int j=0;j<SIZE;j++){
+                board[i][j]='_';
+            }
+        }
+        
+    }
 }
