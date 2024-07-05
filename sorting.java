e08e053d696f40941156519f9abc4e606fe6e710
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-05 16:46:41 +0530

    binary search to search for target in a given sorted array

--- sorting.java
+++ sorting.java
@@ -16,6 +16,23 @@ public class sorting {
                 return i;
             }
         }
+        return -1;
     }
 
+
+    private int binarySearch(int[] array, int num){
+        int start=0;
+        int end=array.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(array[mid]==num){
+                return mid;
+            }else if(num < array[mid]){
+                end=mid-1;
+            }else if(num > array[mid]){
+                start=mid+1;
+            } 
+        }
+        return -1;
+    }
 }
