b3e3e95c7cca2562fd1928aa7205bb9c1d765675
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-05 16:40:28 +0530

    linear search method is implemented to search an given element

--- sorting.java
+++ sorting.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class sorting {
     private int[] array={1,2,4,7,8,33,12,15};
 
@@ -9,8 +10,12 @@ public class sorting {
         this.array=array;
     }
 
-    private int linearSearch(){
-
+    private int linearSearch(int[] array,int num){
+        for(int i=0;i<array.length;i++){
+            if(array[i]==num){
+                return i;
+            }
+        }
     }
 
 }
