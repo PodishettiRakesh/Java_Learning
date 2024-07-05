7fe09a56dc516e609305d95ccb0839eaf5430fd6
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-05 16:37:52 +0530

    deafault constructor is implemented

--- sorting.java
+++ sorting.java
@@ -1,6 +1,14 @@
 public class sorting {
     private int[] array={1,2,4,7,8,33,12,15};
 
+    public sorting(int[] array){
+        this.array=array;
+    }
+
+    public sorting(){
+        this.array=array;
+    }
+
     private int linearSearch(){
 
     }
