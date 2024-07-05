3e21b766e0a5cab9ed2f73aea28bf154985099c5
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-05 11:13:55 +0530

    checkWin method is implemented to check the winning condition

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -83,23 +83,51 @@ public class Tic_Tac_Toe {
         }
     }
 
-    // private void checkWin(){
-    //     int row=0;
-    //     for(int i=0;i<SIZE;i++){
-    //         if(board[row][i]=='_'){
-    //             continue;
-    //         }else{
-    //             break;
-    //         }
-    //     }
-    // }
+    private boolean checkWin(){
+        for(int row=0;row<SIZE;row++){
+            int rowCheck=0;
+            int colCheck=0;
+            for (int col=0;col<SIZE;col++){
+                if(board[row][col]==player_turn){
+                    rowCheck++;
+                }
+                if(board[col][row]==player_turn){
+
+                }
+            }
+            if(rowCheck==SIZE){
+                return true;
+            }else if(colCheck==SIZE){
+                return true;
+            }
+        }
+
+        int rDiagonal=0;
+        for(int i=0;i<SIZE;i++){
+            if(board[i][i]==player_turn){
+                rDiagonal++;
+            }
+        }
+        if(rDiagonal==SIZE){
+            return true;
+        }
+        
+        int lDiagonal=0;
+        for(int i=0;i<SIZE;i++){
+            if(board[i][SIZE-i]==player_turn){
+                lDiagonal++;
+            }
+        }
+        if(lDiagonal==SIZE){
+            return true;
+        }
+        return false;
+    }
+
 
     public static void main(String[] args) {
         Tic_Tac_Toe ttt = new Tic_Tac_Toe();
-        int count = 0;
-        while(count < 5){
-            ttt.makeMove();
-        }
+
     }
 
 }
