aeb6eec207d4e531c6da20aac67d9f16f863002c
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-29 21:38:32 +0530

    compare method  to compare char lexicographically

--- main.java
+++ main.java
@@ -2,8 +2,9 @@ public class main {
     public static void main(String[] args) {
         String str="Rakesh";
         String str1="rakesh";
-        System.out.println(str.substring(3));
-        System.out.println(str.substring(0,3));
+        // System.out.println(str.substring(3));
+        // System.out.println(str.substring(0,3));
+        System.out.println(Character.compare(str.charAt(0), str1.charAt(0)));
     }
     
 }
