76b1c5615f50eda925cf9d29b5c573d0a8b4d17d
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-29 22:15:04 +0530

    counting specific char form a given string

--- main.java
+++ main.java
@@ -17,11 +17,25 @@ public class main {
         // System.out.println(num*2);
 
 
-        int num=98765;
-        String str=String.valueOf(num);
-        for(int i=0; i<str.length(); i++){
-            System.out.println(str.charAt(i));
+        // int num=98765;
+        // String str=String.valueOf(num);
+        // for(int i=0; i<str.length(); i++){
+        //     System.out.println(str.charAt(i));
+        // }
+
+
+        String s="89477873";
+        char c='8';
+        int count=0;
+        for(int i=0; i<s.length(); i++){
+            char d=s.charAt(i);
+            
+            if(Character.compare(d, c)==0){
+                count++;
+            }
         }
+        System.out.println(count);
+
     }
     
 }
