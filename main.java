e785c947fda78788a314d8b881534f07a5d82c9f
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-29 22:03:11 +0530

    String to Integer conversion and performed multiply operation

--- main.java
+++ main.java
@@ -1,10 +1,15 @@
 public class main {
     public static void main(String[] args) {
-        String str="Rakesh";
-        String str1="rakesh";
+        // String str="Rakesh";
+        // String str1="rakesh";
         // System.out.println(str.substring(3));
         // System.out.println(str.substring(0,3));
-        System.out.println(Character.compare(str.charAt(0), str1.charAt(0)));
+        // System.out.println(Character.compare(str.charAt(0), str1.charAt(0)));
+
+        String str= "1234";
+        int num=Integer.parseInt(str);
+        System.out.println(num*2);
+
     }
     
 }
