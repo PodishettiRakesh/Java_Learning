cced2382221d46187111c1aa1990eed3461f62e6
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-12 10:58:50 +0530

    sub class Dog is created and abstrat method is implemented in it

--- Animal.java
+++ Animal.java
@@ -5,3 +5,9 @@ public abstract class Animal {
         System.out.println("The Animal is eating");
     }
 }
+
+class Dog extends Animal{
+    void sound(){
+        System.out.println("dog barks as bow bow");
+    }
+}
